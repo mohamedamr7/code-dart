fa5cfeb9a610d19a0da55d9d2b2ad3ce87e33876
mohamed amr <UG_31135542@ics.tanta.edu.eg>
2022-12-15 09:01:19 +0200

    inital commit

---
+++ src/revisison.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class revisison {
+    static  Scanner input=new Scanner(System.in);
+    public static  void main (String[]args)
+    {
+        int x= input.nextInt();
+        int y=input.nextInt();
+        System.out.println(x*y);
+    }
+}
